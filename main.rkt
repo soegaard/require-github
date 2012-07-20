1523c84d55bde29d6d267f195c2c2a8c7b74e89b
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-20 22:32:04 +0200

    Allow head as commit-id. Branch is optional.

--- main.rkt
+++ main.rkt
@@ -24,9 +24,8 @@
            #:commit-id [commit-id #f]
            #:exists    [exists-mode 'replace])
     (define url (github-zip-url user repo #:branch branch))
-    (define zip-filename (github-zip-saveas-filename user repo branch))
-    (unless (directory-exists? "tmp")
-      (make-directory "tmp"))
+    (define zip-filename (github-zip-saveas-filename user repo branch))    
+    ; make sure repos/ exists
     (unless (directory-exists? "repos")
       (make-directory "repos"))
     (define tmp-path (build-path (current-directory) "tmp"))
@@ -64,33 +63,77 @@
       (delete-directory/files repo-dir)
       ; return the new directory
       (values repo-dir cached-repo-dir))))
-  
+
 (module require-github racket
-  (require (for-syntax (submod ".." download-github))
-           racket/require-syntax)
+  (require racket/require-syntax
+           (for-syntax (submod ".." download-github)
+                       (planet neil/json-parsing:2:0)
+                       net/url))
   (provide github)
+  
+  (begin-for-syntax
+    (define (get-commit-id-for-head user repo [branch "master"])
+      (define url 
+        (string->url
+         (format "https://api.github.com/repos/~a/~a/git/refs/heads/~a"
+                 user repo branch)))
+      (cond
+        [(json->sjson (get-pure-port url))
+         => (λ (tree)
+              (cond [(hash-ref tree 'object)
+                     => (λ (obj) 
+                          (cond [(hash-ref obj 'sha)
+                                 => (λ (s) (substring s 0 7))]
+                                [else #f]))]
+                    [else #f]))]
+        [else #f])))
   
   (define-require-syntax github
     (λ (stx)
-      (syntax-case stx ()
+      (syntax-case stx (head)
+        [(_ user repo head path)
+         (datum->syntax 
+          stx (syntax->datum #'(github user repo master head path)))]
+        [(_ user repo commit-id path)
+         (datum->syntax 
+          stx (syntax->datum #'(github user repo master commit-id path)))]
+        [(_ user repo branch head path)
+         (let-values 
+             ([(user repo branch)
+               (apply values (map syntax->datum (list #'user #'repo #'branch)))])
+           (with-syntax 
+               ([(user repo branch commit-id)
+                 (list user repo branch 
+                       (get-commit-id-for-head user repo branch))])
+             (datum->syntax 
+              stx (syntax->datum #'(github user repo branch commit-id path)))))]
         [(_ user repo branch commit-id path)
-         (let ()
-           (let-values 
-               ([(user repo branch commit-id path)
-                 (apply values (map syntax->datum (list #'user #'repo #'branch #'commit-id #'path)))])
-             (define dir-str (format "repos/~a-~a-~a" user repo commit-id))
-             (define filename-str (format "~a/~a" dir-str path))
-             (unless (directory-exists? (build-path (current-directory) dir-str))
-               (displayln (format "Downloading from GitHub." ))
-               (download-github user repo #:branch branch #:commit-id commit-id)
-               (displayln "Done."))
-             (datum->syntax stx `(file ,filename-str))))]))))
+         (let-values 
+             ([(user repo branch commit-id path)
+               (apply values (map syntax->datum (list #'user #'repo #'branch #'commit-id #'path)))])
+           (define dir-str (format "repos/~a-~a-~a" user repo commit-id))
+           (define filename-str (format "~a/~a" dir-str path))
+           (unless (directory-exists? (build-path (current-directory) dir-str))
+             (displayln (format "Downloading from GitHub." ))
+             (download-github user repo #:branch branch #:commit-id commit-id)
+             (displayln "Done."))
+           (datum->syntax stx `(file ,filename-str)))]
+        [else
+         (raise-syntax-error 
+          #f "Expected (github <user> <repo> <optional-branch> <commit-id or head> <path>)" stx)]))))
 
 (module* test racket
   (require (submod ".." require-github))
+  ; Explicit commit-id is fast. 
+  ; (no http request necessary if the the repository is cached)
   (require (github soegaard this-and-that master faf74b7 "split-between.rkt"))
   (displayln "Running tests in the test submodule.")
-  (split-between (λ (x y) (not (= x y))) '(1 1 2 3 3 4 5 5)))
+  (split-between (λ (x y) (not (= x y))) '(1 1 2 3 3 4 5 5))
+  ; Use head as commit-id to get the latest version.
+  ; This is a bit slow, since a http request to GitHub is neccessary.
+  ; This also shows that the branch defaults to "master".
+  (require (github samth array.rkt head "main.rkt"))
+  (build-array 10 values))
 
 ;(require rackunit)
 ;(check-equal? (url->string
